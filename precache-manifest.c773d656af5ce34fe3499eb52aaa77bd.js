0a05b076e66c57d8765eb3e7e006cecb80b830ae
nightmare364 <adarshjadhav63@gmail.com>
2020-11-13 21:24:31 +0530

    Updates

---
+++ precache-manifest.c773d656af5ce34fe3499eb52aaa77bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01df1ded993d54a48fcbb70104c4e933",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24acb3c11026b2ac47f8",
+    "url": "/static/css/main.c8348206.chunk.css"
+  },
+  {
+    "revision": "9fb38268d7a07868cbc9",
+    "url": "/static/js/2.a9630ba5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a9630ba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24acb3c11026b2ac47f8",
+    "url": "/static/js/main.afd609fc.chunk.js"
+  },
+  {
+    "revision": "ec2b7ed2ec2b268567c0",
+    "url": "/static/js/runtime-main.62bccdd6.js"
+  }
+]);
